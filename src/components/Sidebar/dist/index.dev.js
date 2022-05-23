2911b86e1d2526624485e5e3789c4b2016cb8b0a
hyeon0416 <hyeonsh0416@gmail.com>
2022-05-23 16:34:42 +0900

    [new] Sidebar component

---
+++ src/components/Sidebar/dist/index.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _reactRouterDom = require("react-router-dom");
+
+require("./index.css");
+
+function Sidebar() {}
+
+var _default = Sidebar;
+exports["default"] = _default;
